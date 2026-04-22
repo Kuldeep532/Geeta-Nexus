754788515298c854a9aae06b67de7fcfd67dcc6d
Kuldeep532 <126377510+Kuldeep532@users.noreply.github.com>
2026-04-22 22:15:05 +0530

    Update Android Gradle plugin version to 8.9.1

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -18,7 +18,8 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.7.3" apply false
+    // Yahan 8.7.3 ko badal kar 8.9.1 kar diya gaya hai
+    id("com.android.application") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
