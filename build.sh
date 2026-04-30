e5d85dc62a890652ae8cb1b86fdbeb8746e8eb75
Kuldeep532 <126377510+Kuldeep532@users.noreply.github.com>
2026-04-30 21:18:44 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Flutter SDK setup
-git clone https://github.com/flutter/flutter.git -b stable
-export PATH="$PATH:`pwd`/flutter/bin"
-
-# Project setup aur build
-flutter pub get
-flutter build web --release
